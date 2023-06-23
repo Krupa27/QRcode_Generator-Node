83d151a2362cfb6c3c35a4d1cb31661cbf2d52c5
Krupa_Sagar <91311159+Krupa27@users.noreply.github.com>
2023-06-23 20:31:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const qr = require("qrcode");
+const data={
+    name:"krupasagar",
+    place:"Hyderabad",
+    state:"Telangana",
+    country:"India"
+}
+let stJson = JSON.stringify(data);
+
+qr.toString(stJson,{type:"terminal"})
+.then((err,code)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log(code);
+    }
+})
+
+qr.toFile("qr.png",stJson)
+.then((err)=>{
+    if(err){
+        console.log(err);
+    }
+})
